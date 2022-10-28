c85a330119decefadcd7541c3d82fea79bbab640
Onur Anıl Mertoglu <53944526+onuranl@users.noreply.github.com>
2022-10-29 01:38:10 +0300

    change head title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'chess',
+    title: 'chessTR',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
